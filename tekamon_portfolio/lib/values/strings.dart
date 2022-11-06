b2e18f29df212104b03334a9776e38ef109c8238
Josue Hernández <josue.hernandez@geekinc.mx>
2022-11-06 15:26:53 -0600

    Iniciando adaptaciones a pagina personal

---
+++ tekamon_portfolio/lib/values/strings.dart
@@ -0,0 +1,141 @@
+part of values;
+
+class StringConst {
+  //strings
+  static const String APP_NAME = "Aerium";
+  static const String APP_TITLE = "Josué Hernández Portfolio";
+  static const String SITE_URL = "https://josuehernadez.dev";
+
+  static const String HOME = "Home";
+  static const String ABOUT_ME = "About Me";
+  static const String PORTFOLIO = "Portfolio";
+  static const String EXPERIENCE = "Experience";
+  static const String WORK = "Where I've Worked";
+  static const String SERVICES = "Services";
+  static const String CONTACT = "Contact";
+  static const String PHONE_NUMBER = "Phone Number";
+  static const String RESUME = "Resume";
+  static const String CERTIFICATIONS = "Certifications";
+  static const String EMAIL_VALUE = "josuehernandezr082@gmail.com";
+
+  static const String MESSAGE_ME = "Message me";
+  static const String GET_IN_TOUCH = "Get in touch";
+  static const String VIEW = "View";
+  static const String COMING_SOON = "COMING SOON !!";
+  static const String BUILT_WITH = "Built with ";
+  static const String DEV_EMAIL = "josuehernandezr082@gmail.com";
+  static const String EMAIL_URL = "mailto:<$DEV_EMAIL>?subject=&body=";
+
+  static const String KEY_SKILLS = "KEY SKILLS";
+  static const String EXPERIENCE_2 = "EXPERIENCE";
+  static const String EDUCATION = "EDUCATION";
+
+  static const String SEND_ME_A_MESSAGE = "Send me a message";
+  static const String VIEW_PORTFOLIO = "View Portfolio";
+
+  static const String PROJECT_NAME = "Project Name";
+
+  static const String INTRO = "Hi, my name is";
+  static const String DEV_NAME = "Josué Hernández";
+  static const String PUNCH_LINE = "I build things with flutter";
+  static const String SPECIALITY = "FLUTTER DEV";
+  static const String DESIGNED_IN = "Made in México with ";
+  static const String DESIGNED_BY = "Designed by Pavel G. ";
+  static const String BUILT_BY = "Built by Josué Hernández ";
+
+  static const String ABOUT_DEV_TEXT =
+      "I'm a software engineer based in Mexico, specializing in building high quality mobile with a strong passion for opensource work. My goal is to always build pixel perfect high performant applications that provides users with a rich experience.";
+
+  static const String EDUCATION_TEXT =
+      "I am self taught programmer who graduated from Kwame Nkrumah Univeristy of Science Technology with a degree in Agricultural Engineering. I love learning and writing computer programs.";
+  //Form Hint Texts
+  static const String NAME_HINT_TEXT = "Name";
+  static const String PHONE_HINT_TEXT = "Phone (Optional)";
+  static const String EMAIL_HINT_TEXT = "Email";
+  static const String MESSAGE_HINT_TEXT = "Message";
+
+  static const String SEND_MESSAGE = "Send Message";
+
+  //Skills
+  static const String FLUTTER = "Flutter";
+  static const String JAVA = "Java";
+  static const String ANDROID = "Android";
+  static const String KOTLIN = "Kotlin";
+  static const String JAVASCRIPT = "Javascript";
+  static const String PHP = "PHP";
+  static const String LARAVEL = "Laravel";
+  static const String SQL = "SQL";
+  static const String WORDPRESS = "Wordpress";
+  static const String BOOTSTRAP = "Bootstrap";
+  static const String HTML_CSS = "HTML/CSS";
+
+  //routes
+  static const String HOME_PAGE = "/";
+  static const String ABOUT_PAGE = "/about";
+  static const String PORTFOLIO_PAGE = "/portfolio";
+  static const String CONTACT_PAGE = "/contact";
+  static const String PROJECT_PAGE = "/project";
+  static const String PROJECT_DETAIL_PAGE = "/project-detail";
+  static const String SERVICES_PAGE = "/services";
+  static const String MESSAGE_PAGE = "/message-me";
+  static const String EXPERIENCE_PAGE = "/experience";
+  static const String RESUME_PAGE = "/resume";
+  static const String CERTIFICATION_PAGE = "/certifications";
+
+  //Socials
+  static const String GITHUB_URL = "https://github.com/JosueHernandezR";
+  static const String LINKED_IN_URL =
+      "https://www.linkedin.com/in/josué-hernández/";
+  static const String TWITTER_URL = "https://twitter.com/tekamon117";
+  static const String INSTAGRAM_URL = "";
+  static const String TELEGRAM_URL = "";
+
+  //Contacts
+  static const String CONNECT = "Connect with me";
+  static const String EMAIL = "Email";
+  static const String LINKED_IN = "LINKEDIN";
+  static const String TWITTER = "TWITTER";
+  static const String INSTAGRAM = "INSTAGRAM";
+  static const String TELEGRAM = "TELEGRAM";
+
+  //Certificate Urls
+  static const String ASSOCIATE_ANDROID_DEV_URL = "";
+  static const String DATA_SCIENCE_CERT_URL = "";
+  static const String ANDROID_BASICS_CERT_URL = "";
+
+  //AwardedBy
+  static const String GOOGLE = "Google";
+  static const String UDACITY = "Udacity";
+
+  //CertificationType
+  static const String ASSOCIATE_ANDROID_DEV = "Associate Android Developer";
+  static const String DATA_SCIENCE = "Data Science";
+  static const String ANDROID_BASICS = "Android Basics";
+
+  //Experience
+  static const String CURRENT_MONTH_YEAR = "May 2020 - Current";
+  static const String STARTED_MONTH_YEAR = "May 2017";
+
+  static const String COMPANY_1 = "";
+  static const String COMPANY_1_URL = "";
+  static const String POSITION_1 = "";
+  static const String COMPANY_1_ROLE_1 = "";
+  static const String COMPANY_1_ROLE_2 = "";
+  static const String COMPANY_1_ROLE_3 = "";
+  static const String LOCATION_1 = "";
+  static const String DURATION_1 = "";
+
+  //Portfolio, Projects
+  static const String LOGIN_CATALOG = "Login Catalog";
+  static const String LOGIN_CATALOG_SUBTITLE =
+      "A beautiful catalog of login designs";
+  static const String LOGIN_CATALOG_DETAIL =
+      "Login Catalog is a beautiful catalog of login designs built with flutter. The goal is to catalog as many as a 1000 different login/Signup designs to help flutter devs get right to the most important part of their app. Another reason behind the project is to show that flutter can be used to build beautiful and exquisite UI.";
+  static const String LOGIN_CATALOG_GITHUB_URL =
+      "https://github.com/david-legend/login_catalog";
+
+  //API
+  static const String BASE_URL =
+      "https://asqbx1u5bg.execute-api.us-east-1.amazonaws.com/production/";
+  static const String SEND_END_POINT = "send";
+}
